d620dfcd252bca73a2b01e4bad0506c94d026e68
abinavbaskar <abinav.baskar@gmail.com>
2020-05-01 10:29:09 +0100

    changed server to app

--- server.js
+++ server.js
@@ -19,11 +19,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 }); 
 
-
+app.listen( app.get( 'port' ), function() {
+    console.log( 'Node server is running on port ' + app.get( 'port' ));
+    });
+    /*
   server.listen(port, function () {
     console.log(`(2)Listening on ${server.address().port}`);
   });
- 
+ */
 
 const gameStages = {
 	newGame: 0,
